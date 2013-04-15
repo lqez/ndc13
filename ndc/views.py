83064af2994b73f7308a1d7b739905f4981c3aed
Park Hyunwoo <ez@smartstudy.co.kr>
2013-04-15 10:48:57 +0900

    Add table view

--- ndc/views.py
+++ ndc/views.py
@@ -8,7 +8,29 @@ from ndc.models import *
 
 
 def home(request):
-    return render(request, 'home.html')
+    dates = SessionDate.objects.all()
+    times = SessionTime.objects.all()
+    rooms = Room.objects.all()
+
+    table = {}
+    for d in dates:
+        table[d] = {}
+        for t in times:
+            table[d][t] = {}
+            for r in rooms:
+                s = Session.objects.filter(date=d, times=t, room=r)
+                if s:
+                    if s[0].times.all()[0] == t:
+                        table[d][t][r] = s[0]
+                else:
+                    table[d][t][r] = None
+
+    return render(request, 'home.html', {
+        'dates': dates,
+        'times': times,
+        'rooms': rooms,
+        'table' : table,
+    })
 
 
 class speaker_list(ListView):
@@ -25,4 +47,3 @@ class session_list(ListView):
 
 class session_detail(DetailView):
     model = Session
-
